f9c6b91482d0f034ad54e8689ee911e9128eef70
nadavyer <nadavyerushalmi@gmail.com>
2020-03-25 18:35:59 +0200

    1.3 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -24,8 +24,18 @@ const Content = ({good, neutral, bad}) => {
         neutral {neutral}<br/>
         bad {bad}<br/>
         average {(good - bad) / (good + neutral + bad)}<br/>
-        positive {good / (good + neutral + bad)} %<br/>
+        positive {(good / (good + neutral + bad)) * 100} %<br/>
       </p>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  
+  return (
+    <div>
+      <Header text={props.header} />
+      <Content good={props.good} neutral={props.neutral} bad={props.bad} />
     </div>
   )
 }
@@ -35,7 +45,6 @@ const App = () => {
 
   const header1 = 'give feedback'
   const header2 = 'statistics'
-
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -60,11 +69,12 @@ const App = () => {
         <Button onClick={handleGoodClick} text='good' />
         <Button onClick={handleNeutralClick} text='neutral' />
         <Button onClick={handleBadClick} text='bad' />
-        <Header text={header2} />
-        <Content good={good} neutral={neutral} bad={bad} />
+        <Statistics header={header2} good={good} bad={bad} neutral={neutral} />
       </div>
   )
 }
+
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
