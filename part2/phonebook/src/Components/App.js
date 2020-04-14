60f7d7d2bf25d535a64656f1218e6f2866d9f430
nadavyer <nadavyerushalmi@gmail.com>
2020-04-14 16:06:11 +0300

    2.18: pgonebook step10 - need to check validators

--- part2/phonebook/src/Components/App.js
+++ part2/phonebook/src/Components/App.js
@@ -21,54 +21,83 @@ const App = () => {
   }
 
   useEffect(getAllPersons, [])
+    
+  const updatePerson = ({ origPerson }) => {
+    
+    const updatedPerson = {...origPerson, number: number}
+    personsServices
+    .update(updatedPerson.id, updatedPerson)
+    .then(personToSet => 
+      setPersons(persons.map(person => person.id !== origPerson.id ? person : personToSet)))
+  }
+
+  const validateName = () => {
+    const reg = /^[a-zA-Z ]*$/
+    const trimedName = name.trim()
+    if (!reg.test(trimedName) || trimedName === '') {
+      window.alert(`"${name}" is not a valid name`)
+      return false;
+   }
+    return true;
+  }
+
+  const validateNumber = () => {
+    const reg = /^[0-9]*$/
+    const trimedNumber = number.trim()
+    if (!reg.test(trimedNumber) || trimedNumber === '') {
+      window.alert(`"${number}" is not a valid number`)
+      return false;
+   }
+    return true;
+  }
+  
 
   const addPerson = (event) => {
     event.preventDefault()
-    if(persons.map(person => person.name).includes(name)) {
-        window.alert(`${name} is already added to phonebook`)
+    if(persons.map(person => person.name).includes(name) && validateName() && validateNumber()) {
+        if (window.confirm(`${name} is already added to phonebook, replace old number with a new one?`)) {
+          const origPerson = persons.find(p => p.name === name)
+          updatePerson(origPerson)
+        }
     }else {
-      const personObj = {name, number}
-      personsServices
-      .create(personObj)
-      .then(addedPerson => { 
-        setPersons(persons.concat(addedPerson))
-        setName('')
-        setNumber('')
-      })   
-    }
-  }
-
-
-const handleDeleteBtn = (id) => {
-  const personToDel = persons.find(person => person.id === id)
-  if (window.confirm(`Delete ${personToDel.name}?`)){
-    personsServices
-  .remove(id)
-  .then(setPersons(persons.filter(person =>
-    person.id !== id)))
-  }
-
-}
-
-
-const handleNameChange = (event) => {
-  const re = /^[0-9\b]+$/;
-      if (event.target.value === '' || re.test(event.target.value)) {
-        setName(event.target.value)
+      if (validateName() && validateNumber() ) {
+        const personObj = {name, number}
+          personsServices
+          .create(personObj)
+          .then(addedPerson => 
+            setPersons(persons.concat(addedPerson)))
       }
+    }
+    setName('')
+    setNumber('')  
+  }
   
-}
 
-const handleNumberChange = (event) => {
-  setNumber(event.target.value)
-}
+  const handleDeleteBtn = (id) => {
+    const personToDel = persons.find(person => person.id === id)
+    if (window.confirm(`Delete ${personToDel.name}?`)){
+      personsServices
+    .remove(id)
+    .then(setPersons(persons.filter(person =>
+      person.id !== id)))
+    }
 
-const handleFilterChange = (event) => {
-  setFilter(event.target.value)
-}
+  }
 
-const filteredPersons =  filter === '' ? persons : persons.filter(person =>
-   person.name.toLowerCase().includes(filter.toLowerCase()))
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons =  filter === '' ? persons : persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase()))
 
 
   return (
@@ -77,12 +106,13 @@ const filteredPersons =  filter === '' ? persons : persons.filter(person =>
       <Filter value={filter} onChange={handleFilterChange} />
       <h2>Add a new</h2>
       <PersonForm onSubmit={addPerson} nameValue={name} onNameChange={handleNameChange}
-       numberValue={number} onNumberChange={handleNumberChange} />
+      numberValue={number} onNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <PersonsList filteredPersons={filteredPersons} handleDeleteBtn={handleDeleteBtn} />
         
     </div>
   )
 }
+
 
 export default App
