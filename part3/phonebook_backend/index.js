4500c5a6716962e73cdaba0e26cea8b091eb1a55
nadavyer <nadavyerushalmi@gmail.com>
2020-04-19 21:25:06 +0300

    3.16: phonebook backend step4 - need to do PUT on back and front and add error handlers

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,40 +1,25 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+const logger = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const utills = require('./utills')
+const Person = require('./models/person')
 
 const app = express()
-app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
-morgan.token('person', (request, response) => {
+app.use(cors())
+
+
+//clog all POST 
+logger.token('person', function (request, response) {
   return JSON.stringify(request.body)
 })
-//clog all POST 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person', {
-  skip: function (req, res) {return req.method !== 'POST'}
+app.use(logger(':method :url :status :res[content-length] - :response-time ms :person', {
+  skip: function (request, response) {return request.method !== 'POST'}
 }))
-app.use(express.json())
-// app.use(express.static('build'))
-const password = process.argv[2]
 
-const url =
-  `mongodb+srv://nadavyer:010203@fullstackopen1-ohe7j.mongodb.net/test?retryWrites=true&w=majority`
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
-})
 
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-const Person = mongoose.model('Person', personSchema)
 
 let persons = [
     { 
@@ -59,51 +44,56 @@ let persons = [
     }
   ]
 
-  let maxId = persons.length + 1
 
-
-app.get('/info', (req, res) => {
+app.get('/info', (request, response) => {
   console.log(utills.getCurrentTime())
-  res.end(`Phonebook has info for ${persons.length} people \n\n${utills.getCurrentTime()} (GMT+3 Middle East Time Zone)`)
+  response.end(`Phonebook has info for ${persons.length} people \n\n${utills.getCurrentTime()} (GMT+3 Middle East Time Zone)`)
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()))
+    response.json(persons.map(person => person.toJSON()))
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  person ? res.json(person) : res.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person.toJSON())
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    persons = persons.filter(p => p.id !== id)
-    res.json(persons)
-  }
-  else {
-    res.status(204).end()
-  }
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
-  const person = req.body
-  console.log(person)
-  if (!utills.validAddInput(person)) {
-    res.send({error: 'name or number is missing'})
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  if (!utills.validAddInput(body)) {
+    response.status(400).json({ error: 'name or number is missing' })
   }
-  else if (utills.nameInPhonebook(person.name, persons)) {
-    res.send({error: 'name must be unique'})
+  else if (utills.nameInPhonebook(body.name, persons)) {
+    response.send({ error: 'name must be unique' })
   }
   else {
-    person.id = maxId++
-    persons = persons.concat(person)
-    res.json(person)
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+    person.save().then(savedPerson => {
+      response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
   }
 })
 
@@ -113,7 +103,19 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
