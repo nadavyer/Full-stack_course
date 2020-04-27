0fc7eaaeae9300940e87b88c235c21f76985ef6b
nadavyer <nadavyerushalmi@gmail.com>
2020-04-27 20:55:16 +0300

    4.8: blogs tests, step1 - completed

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    { 
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+    },
+    { 
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    },
+    {
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    },
+     {
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+    },
+     { 
+     title: "Type wars",
+     author: "Robert C. Martin",
+     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+     likes: 2, 
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let i = 0; i < initialBlogs.length; i++) {
+        let blogObj = new Blog(initialBlogs[i])
+    await blogObj.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-type', /application\/json/)
+})
+
+test(`there are ${initialBlogs.length} blogs`, async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog in the blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(t => t.title)
+    expect(titles).toContain('First class tests')
+})
+
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
+
